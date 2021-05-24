b64518bd15d03a47350a787d19e49431095c03fe
Shobhit Mittal <mittalshobhit07@gmail.com>
2021-05-24 23:43:23 +0700

    Initial commit

---
+++ harambe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace harambe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
